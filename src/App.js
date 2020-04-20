54c0c27aac87d5d1a3dd8ce162da9544d2c1687b
Iancu Andrei <29271791+iancuandrei@users.noreply.github.com>
2020-04-20 15:39:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import React, { Component } from 'react';
-import TabSelector from './TabSelector';
-import './App.css';
+import React, { Component } from "react";
+import TabSelector from "./TabSelector";
+import "./App.css";
 
 const Home = () => <p>This is the Home component!</p>;
 const About = () => <p>This challenge is about tabs!</p>;
 const Contact = () => <p>Feel free to contact us!</p>;
-const Error = () => <p>Something went <strong>wrong</strong>!</p>;
+const Error = () => (
+	<p>
+		Something went <strong>wrong</strong>!
+	</p>
+);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeId: 'home'
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeId: "home",
+		};
+	}
 
-  getTabContent() {
-    switch(this.state.activeId) {
-      case 'home':
-        return <Home />;
-      default:
-        return <Error />;
-    }
-  }
+	handleChangeTab = (event) => {
+		this.setState({ activeId: event.target.id });
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <TabSelector />
-        <div className="App-content">
-          {
-            this.getTabContent()
-          }
-        </div>
-      </div>
-    );
-  }
+	getTabContent() {
+		switch (this.state.activeId) {
+			case "home":
+				return <Home />;
+			case "contact":
+				return <Contact />;
+			case "about":
+				return <About />;
+			default:
+				return <Error />;
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<TabSelector activeId={this.state.activeId} handleChangeTab={this.handleChangeTab} />
+				<div className="App-content">{this.getTabContent()}</div>
+			</div>
+		);
+	}
 }
 
 export default App;
