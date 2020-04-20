b93f17adc9ab4be3ec43a03fbd170696ebef0ba7
Iancu Andrei <29271791+iancuandrei@users.noreply.github.com>
2020-04-20 15:56:50 +0300

    Updates

---
+++ precache-manifest.8b130d3d839977f8b49721b0550d3087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a463a7618a4108417cdcdfbbac67173d",
+    "url": "/react-12-challenge/index.html"
+  },
+  {
+    "revision": "f9c7a55234bb021fa343",
+    "url": "/react-12-challenge/static/css/main.656f0744.chunk.css"
+  },
+  {
+    "revision": "380f3c94c5f27fc2791d",
+    "url": "/react-12-challenge/static/js/2.5fc70593.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-12-challenge/static/js/2.5fc70593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c7a55234bb021fa343",
+    "url": "/react-12-challenge/static/js/main.a331d011.chunk.js"
+  },
+  {
+    "revision": "d094bcbe29f46548f75f",
+    "url": "/react-12-challenge/static/js/runtime-main.703faee2.js"
+  }
+]);
